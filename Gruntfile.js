b071cb40aa1d44ded031944abe9847cf9585b076
Rafal Lindemann <rl@stamina.pl>
2014-04-21 22:59:53 +0200

    favicons

--- Gruntfile.js
+++ Gruntfile.js
@@ -302,7 +302,7 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>',
           dest: '<%= yeoman.dist %>',
           src: [
-            '*.{ico,png,txt}',
+            '*.{ico,png,gif,jpg,txt}',
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
