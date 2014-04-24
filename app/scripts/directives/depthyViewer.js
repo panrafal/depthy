4863d5d0ca1dda1c5e12013eaa9f31d115e5522e
Rafal Lindemann <rl@stamina.pl>
2014-04-24 09:42:21 +0200

    refactor (ongoing, working)

--- app/scripts/directives/depthyViewer.js
+++ app/scripts/directives/depthyViewer.js
@@ -7,42 +7,46 @@ angular.module('depthyApp')
     restrict: 'A',
     scope: true,
     /**
-      compoundSource
-      imageSource
-      depthSource
-      ready (optional)
-      dirty (optional)
       imageSize (set)
       depthSize (set)
       stageSize (set)
     */
     controller: function($scope, $element, $attrs) {
-      var viewer = $scope.viewer = $scope.$parent.$eval($attrs.depthyViewer);
+      var viewer = $scope.viewer = $scope.$parent.$eval($attrs.depthyViewer),
+          imageTexture, depthTexture, sprite, depthFilter,
+          orientation = {},
+          easedOffset = {x: 0, y: 0};
 
       _.defaults(viewer, {
+        compoundSource: null,
+        imageSource: null,
+        depthSource: null,
+        // set it when all sources are ready (they may be loaded earlier)
+        ready: true,
+        dirty: null,
         useCompoundImage: true,
-      })
+        animate: false,
+        animDuration: 1,
+        animPosition: null,
+        animScale: {x: 1, y: 1},
+        depthScale: 1,
+        offset: {x: 0, y: 0},
+        easeFactor: Modernizr.mobile ? 0.2 : 0.9,
+        update: 1,
+      });
 
       $scope.stage = null;
       $scope.sizeDirty = 0;
 
-      $scope.animate = false;
-      $scope.scaleUp = false;
-      $scope.update = 1;
-      $scope.depthFilter = null;
-      $scope.offset = {x: 0, y: 0};
-      $scope.easedOffset = {x: 0, y: 0};
-      $scope.easeFactor = Modernizr.mobile ? 0.2 : 0.9;
-
 
       function setupStage(stage, renderer) {
-        var imageTexture, depthTexture, sprite, depthFilter;
+
 
         function resetStage() {
           if (sprite) {
             stage.removeChild(sprite);
             sprite = null;
-            $scope.update = 1;
+            viewer.update = 1;
           }
         }
 
@@ -79,12 +83,12 @@ angular.module('depthyApp')
 
         }, true);
 
-        $scope.$watch('[viewer.imageSize, viewer.depthSize, sizeDirty]', function() {
-          if (!viewer.imageSize || !viewer.depthSize) return;
+        $scope.$watch('[viewer.imageSize, viewer.depthSize, viewer.ready, sizeDirty]', function() {
+          if (!viewer.imageSize || !viewer.depthSize || !viewer.ready) return;
 
           var imageSize = viewer.imageSize,
-            imageRatio = imageSize.width / imageSize.height,
-            stageSize = {width: imageSize.width, height: imageSize.height};
+              imageRatio = imageSize.width / imageSize.height,
+              stageSize = {width: imageSize.width, height: imageSize.height};
 
           if (stageSize.height > $($window).height() * 0.8) {
             stageSize.height = Math.round($($window).height() * 0.8);
@@ -111,10 +115,10 @@ angular.module('depthyApp')
           viewer.stageSize = stageSize;
         }, true);
 
-        $scope.$watch('[viewer.stageSize, viewer.dirty]', function() {
+        $scope.$watch('[viewer.stageSize, viewer.dirty, viewer.ready]', function() {
           resetStage();
 
-          viewer.loaded = !viewer.error && imageTexture && depthTexture && viewer.imageSize && viewer.depthSize && viewer.stageSize;
+          viewer.loaded = !viewer.error && imageTexture && depthTexture && viewer.imageSize && viewer.depthSize && viewer.stageSize && viewer.ready;
 
           if (!viewer.loaded) return;
 
@@ -127,10 +131,10 @@ angular.module('depthyApp')
 
           sprite = new PIXI.Sprite(imageTexture);
 
-          var depthScale = (Modernizr.mobile ? 0.015 : 0.015) * ($scope.scaleUp ? 2 : 1);
-          $scope.depthFilter = depthFilter = new PIXI.DepthmapFilter(depthTexture);
+          var depthScale = (Modernizr.mobile ? 0.015 : 0.015) * (viewer.depthScale || 1);
+          depthFilter = new PIXI.DepthmapFilter(depthTexture);
           depthFilter.scale = {
-            x: (stageSize.width > stageSize.height ? 1 : stageSize.height / stageSize.width) * depthScale, 
+            x: (stageSize.width > stageSize.height ? 1 : stageSize.height / stageSize.width) * depthScale,
             y: (stageSize.width < stageSize.height ? 1 : stageSize.width / stageSize.height) * depthScale
           };
 
@@ -138,72 +142,64 @@ angular.module('depthyApp')
           sprite.scale = new PIXI.Point(stageScale, stageScale);
           stage.addChild(sprite);
           //render on load events
-          $scope.update = 1;
+          viewer.update = 1;
         }, true);
 
         $element.on('mousemove touchmove', function(e) {
-          
-          if ($scope.animate) return;
+          if (viewer.animate || angular.isNumber(viewer.animPosition)) return;
 
           var elOffset = $element.offset(),
-            elWidth = $element.width(),
-            elHeight = $element.height(),
-            stageSize = viewer.stageSize.height * 0.8,
-            pointerEvent = e.originalEvent.touches ? e.originalEvent.touches[0] : e,
-            x = (pointerEvent.pageX - elOffset.left) / elWidth,
-            y = (pointerEvent.pageY - elOffset.top) / elHeight
+              elWidth = $element.width(),
+              elHeight = $element.height(),
+              stageSize = viewer.stageSize.height * 0.8,
+              pointerEvent = e.originalEvent.touches ? e.originalEvent.touches[0] : e,
+              x = (pointerEvent.pageX - elOffset.left) / elWidth,
+              y = (pointerEvent.pageY - elOffset.top) / elHeight;
 
           x = Math.max(-1, Math.min(1, (x * 2 - 1) * elWidth / stageSize));
           y = Math.max(-1, Math.min(1, (y * 2 - 1) * elHeight / stageSize));
 
           if (depthFilter) {
-            $scope.offset = {x: -x, y: -y};
-            $scope.update = 1;
+            viewer.offset = {x: -x, y: -y};
+            viewer.update = 1;
           }
           
-        })
+        });
 
-        var orientation = {}
         $window.addEventListener('deviceorientation', function(event) {
           if (event.beta === null || event.gamma === null) return;
+          if (viewer.animate || angular.isNumber(viewer.animPosition)) return;
 
-          if (orientation && !$scope.animate) {
+          if (orientation) {
             var portrait = window.innerHeight > window.innerWidth,
-              beta = (event.beta - orientation.beta) * 0.2,
-              gamma = (event.gamma - orientation.gamma) * 0.2,
-              x = portrait ? -gamma : -beta,
-              y = portrait ? -beta : -gamma
+                beta = (event.beta - orientation.beta) * 0.2,
+                gamma = (event.gamma - orientation.gamma) * 0.2,
+                x = portrait ? -gamma : -beta,
+                y = portrait ? -beta : -gamma;
 
-            if (depthFilter) {
-
-              if (x && y) {
-                $scope.offset = {
-                  x : Math.max(-1, Math.min(1, $scope.offset.x + x)), 
-                  y : Math.max(-1, Math.min(1, $scope.offset.y + y))
-                };           
-              }         
-              // console.log("offset %d %d ABG %d %d %d", $scope.offset.x, $scope.offset.y, event.alpha, event.beta, event.gamma)
-              $scope.update = 1;
+            if (x && y) {
+              viewer.offset = {
+                x : Math.max(-1, Math.min(1, viewer.offset.x + x)),
+                y : Math.max(-1, Math.min(1, viewer.offset.y + y))
+              };
             }
+            // console.log("offset %d %d ABG %d %d %d", viewer.offset.x, viewer.offset.y, event.alpha, event.beta, event.gamma)
+            viewer.update = 1;
 
           }
           orientation = {
             alpha: event.alpha,
             beta: event.beta,
             gamma: event.gamma
-          }
-        })
-
-        // $window.addEventListener('devicemotion', function(event) {
-        //     console.log(event.acceleration, event.rotationRate)
-        // })
+          };
+        });
 
         $window.addEventListener('resize', function() {
           $scope.sizeDirty ++;
           $scope.$apply();
-        })
+        });
 
-      }   
+      }
 
 
       var stageReady = false;
@@ -215,58 +211,63 @@ angular.module('depthyApp')
           return;
         }
 
-        //should be based on window.performance.now() but maybe later...
-        if ($scope.offset.x != $scope.easedOffset.x || $scope.offset.y != $scope.easedOffset.y) {
+        if (viewer.offset.x !== easedOffset.x || viewer.offset.y !== easedOffset.y) {
           
-          $scope.easedOffset.x = $scope.easedOffset.x * $scope.easeFactor + $scope.offset.x * (1-$scope.easeFactor);
-          $scope.easedOffset.y = $scope.easedOffset.y * $scope.easeFactor + $scope.offset.y * (1-$scope.easeFactor);
-          if (Math.abs($scope.easedOffset.x - $scope.offset.x) < 0.0001 && Math.abs($scope.easedOffset.y - $scope.offset.y) < 0.0001) {
-            $scope.easedOffset = $scope.offset;
+          if (viewer.easeFactor && !angular.isNumber(viewer.animPosition)) {
+            easedOffset.x = easedOffset.x * viewer.easeFactor + viewer.offset.x * (1-viewer.easeFactor);
+            easedOffset.y = easedOffset.y * viewer.easeFactor + viewer.offset.y * (1-viewer.easeFactor);
+            if (Math.abs(easedOffset.x - viewer.offset.x) < 0.0001 && Math.abs(easedOffset.y - viewer.offset.y) < 0.0001) {
+              easedOffset = viewer.offset;
+            }
+          } else {
+            easedOffset = viewer.offset;
           }
 
-          $scope.depthFilter.offset = {
-            x : $scope.easedOffset.x, 
-            y : $scope.easedOffset.y
-          } 
-          $scope.update = 1;
+          depthFilter.offset = {
+            x : easedOffset.x,
+            y : easedOffset.y
+          };
+          viewer.update = 1;
         }
 
-        if ($scope.animate) {
-          var now = Modernizr.performance ? window.performance.now() : new Date();
-          $scope.depthFilter.offset = {
-            x : Math.sin(now * Math.PI / 1000), 
-            y : Math.cos(now * Math.PI / 1000)
-          }            
-          $scope.update = 1;
+        if (viewer.animate || angular.isNumber(viewer.animPosition)) {
+          var now = angular.isNumber(viewer.animPosition) ?
+                      viewer.animPosition * 1000 
+                      : (Modernizr.performance ? window.performance.now() : new Date().getTime());
+          depthFilter.offset = {
+            x : Math.sin(now * Math.PI / viewer.animDuration / 1000) * viewer.animScale.x,
+            y : Math.cos(now * Math.PI / viewer.animDuration / 1000) * viewer.animScale.y
+          };
+          viewer.update = 1;
         }
 
 
-        if (!$scope.update) {
+        if (!viewer.update) {
           return false;
         }
 
-        $scope.update--;
-      }
+        viewer.update--;
+      };
 
 
-      $scope.$on('popover.show', function() {
-        console.log('gif popover');
-      })
+      // $scope.$on('popover.show', function() {
+      //   console.log('gif popover');
+      // });
 
-      $scope.gifExportSetup = function(event) {
-        var popover = $popover($(event.currentTarget), {
-          placement: 'top',
-          trigger: 'manual',
-          title: 'How do you want your GIF?',
-          contentTemplate: "views/gif-popover.html",
-        })
-        popover.$promise.then(function() {popover.show()})
-      }
+      // $scope.gifExportSetup = function(event) {
+      //   var popover = $popover($(event.currentTarget), {
+      //     placement: 'top',
+      //     trigger: 'manual',
+      //     title: 'How do you want your GIF?',
+      //     contentTemplate: "views/gif-popover.html",
+      //   })
+      //   popover.$promise.then(function() {popover.show()})
+      // }
 
     },
     link: function postLink() {
 
     }
-  }
+  };
 
 });
